25f5b80da8dea6e7d7630002f753b08c6fa03ed7
pabloocaldeira <40439152+pabloocaldeira@users.noreply.github.com>
2018-10-09 09:52:16 -0300

    Update Thread_SMS.py

--- .gitignore/Thread_SMS.py
+++ .gitignore/Thread_SMS.py
@@ -1,4 +1,15 @@
 import serial
+
+    """
+    Ao clicar em um botao, eu chamo a funcao fn_SMS que envia um SMS para o meu numero
+    Problemas: o código nesta seção não é executado de maneira paralela, ou seja, todo o resto (UI e Threads)
+    ficam no aguardo do término da execução dessa função. É uma espera rápida, porém perceptível na interface gráfica.
+    Mas está funcionando corretamente, pois o SMS chega pra mim.
+    
+    Após o envio deste SMS eu inicio uma thread que fica lendo a serial na espera de um recebimento de SMS. Isso sim está funcionano
+    de maneira paralela.
+    """
+
     def fn_SMS(self):
         SERIAL_PORT = "/dev/ttyS0"  # Rasp 3 UART Port
         global ser
@@ -10,7 +21,7 @@ import serial
 
         ser.write("AT+CMGS=\"")
         time.sleep(0.1)
-        ser.write("+5553981075696")
+        ser.write("+5553999999999")
         ser.write("\"\r")
         time.sleep(0.1)
         ser.write("Aguardando SMS")
@@ -22,6 +33,20 @@ import serial
         self.T_SMS = ThreadSMS()
         self.T_SMS.start()
         
+        
+"""
+Meu problema dentro da Thread eh o seguinte:
+Thread Inicia: OK
+Aguardo por SMS: OK
+Recebo o SMS: OK
+Faco o reconhecimento do sms contendo "MECA": OK
+Envio um SMS: OK
+Limpo a serial: ? (acho que nao estou fazendo certo)
+Continuo verificando a serial em looping... porem na leitura da serial, o SMS continua na serial e entrando
+no 'if "MECA" in self.TEXT.upper():'
+
+Existe uma maneira de eu limpar a serial, ou ainda fechar e abrir novamente?
+"""        
 class ThreadSMS(QtCore.QThread):
     TEXT = "vazio"
     def __init__(self):
@@ -47,7 +72,7 @@ class ThreadSMS(QtCore.QThread):
                 time.sleep(0.1)
                 ser.write("AT+CMGS=\"") #enviar sms...
                 time.sleep(0.1)
-                ser.write("+5553981075696") #...para o numero
+                ser.write("+5553999999999") #...para o numero
                 ser.write("\"\r")
                 time.sleep(0.1)
                 ser.write("FEITO") #...texto do sms
